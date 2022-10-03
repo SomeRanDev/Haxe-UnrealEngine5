9d0ddc09e4b501c31d0b64fdfe54554e68b74eb2
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2022-10-03 00:37:41 -0400

    Fixed HaxePlugin compiling issues

--- SampleProject/HaxePlugin/Haxe/src/Test.hx
+++ SampleProject/HaxePlugin/Haxe/src/Test.hx
@@ -2,19 +2,5 @@ package;
 
 @:keep
 @:nativeGen
-@:native("TestCppiaThing")
 class Test {
-	public static var module: Dynamic;
-
-	public static function loadModule(path: String): Void {
-		Main.ExportHaxe.BootAll();
-		var source = sys.io.File.getBytes("Z:\\Desktop\\cppiatest\\bin\\script.cppia");
-		var module = cpp.cppia.Module.fromData(source.getData());
-		module.boot();
-		Test.module = module;
-	}
-
-	public static function getClass(name: String): Dynamic {
-		return module.resolveClass(name);
-	}
 }
